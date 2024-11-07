1b361f3587f87d41ae6ff8f6e094e9bb899f6907
Ch. Raghu Chandra Reddy <123090081+ChallaRaghu@users.noreply.github.com>
2024-11-07 12:27:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId='ami-04a37924ffe27da53',
+    InstanceType='t2.micro',
+    KeyName='mykey',
+    MaxCount=1,
+    MinCount=1
+
+)
